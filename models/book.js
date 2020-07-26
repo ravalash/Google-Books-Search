21d2ad05370b926663d0a96e338a9ff7973028e3
Ravalash <13k0873juhky@opayq.net>
2020-07-26 17:03:38 -0400

    Updating mongoose and schema definition

---
+++ models/book.js
@@ -0,0 +1,31 @@
+//Require mongoose and establish schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Define Book schema
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: "Book requires a title",
+    trim: true,
+  },
+  authors: [String],
+  description: {
+    type: String,
+    required: "Book requires a description",
+  },
+  image: {
+    type: String,
+    lowercase: true,
+    required: "Book requires an image link",
+  },
+  link: {
+    type: String,
+    lowercase: true,
+    required: "Book requires a link",
+  },
+});
+
+//Export Book
+const Book = mongoose.model("Book", bookSchema);
+module.exports = Book;
