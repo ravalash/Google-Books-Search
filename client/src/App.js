82abbea5de856590aa901299fa1cad2777c40185
Ravalash <13k0873juhky@opayq.net>
2020-07-26 17:45:21 -0400

    Laying out grid components and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Switch>
+            {/* <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/portfolio" component={Portfolio} /> */}
+            // <Route component={Search} />
+          </Switch>
+        </Wrapper>
+        <Footer />
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
 
 
 export default App;
+
+
---
+++ client/src/pages/Search.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Info from "../components/About";
+import Container from "../components/Container";
+import Col from "../components/Col";
+import Row from "../components/Row";
+
+function About() {
+  return (
+    <Container>
+      REACT!!!
+      <Row>
+        <Col size="lg-9">SEARCH PAGE!!!</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default About;
