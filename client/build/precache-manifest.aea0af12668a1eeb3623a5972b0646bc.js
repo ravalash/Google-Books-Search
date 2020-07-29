62a881352d757c03ea82c5be382ead092db09e68
Ravalash <13k0873juhky@opayq.net>
2020-07-29 13:58:25 -0400

    pushing build folder

---
+++ client/build/precache-manifest.aea0af12668a1eeb3623a5972b0646bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b397f896c8b66fa00b43",
+    "url": "/static/css/main.75ec1b45.chunk.css"
+  },
+  {
+    "revision": "b397f896c8b66fa00b43",
+    "url": "/static/js/main.64aa3f22.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6966bdfc78f29e15d98",
+    "url": "/static/js/2.9d8f9af6.chunk.js"
+  },
+  {
+    "revision": "c4ab759fc3959e9f2e4d86f7a1afb645",
+    "url": "/index.html"
+  }
+];
